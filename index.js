5f231efcb506520cff5df6f396bf6d6a67722970
Adriana Zestroiu <adriana.zestroiu@yahoo.com>
2016-08-24 21:33:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+var name=["Milo","Otis","Garfield"];
+function destructivelyAppendKitten(name){
+name.push("Ralph");
+return kittens;
+}
+function destructivelyPrependKitten(name){
+  var name=["Milo","Otis","Garfield"];
+  name.unshift("Bob");
+  return name;
+}
+function destructivelyRemoveLastKitten(){
+  var name=["Milo","Otis","Garfield"];
+  name.pop();
+  return name;
+}
+function destructivelyRemoveFirstKitten(){
+  var name=["Milo","Otis","Garfield"];
+  name.shift();
+  return name;
+}
+function appendKitten(name){
+  var name=["Milo","Otis","Garfield"];
+  return ([...name,"Broom"]);
+}
+function prependKitten(name){
+  var name=["Milo","Otis","Garfield"];
+  return (["Arnold",...name]);
+}
+function removeLastKitten(){
+  var name=["Milo","Otis","Garfield"];
+  return name.slice(0,name.length-1);
+}
+function removeFirstKitten(){
+  var name=["Milo","Otis","Garfield"];
+  return name.slice(-2);
+}
