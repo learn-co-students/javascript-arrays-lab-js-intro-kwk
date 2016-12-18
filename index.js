b772165c55247cc41c86aa51de4e8764abf92845
Miranda Bashore <mcbashore@gmail.com>
2016-12-18 08:04:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+];
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+  return kittens;
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(name) {
+  var cats = [...kittens, 'Broom'];
+  return cats;
+}
+function prependKitten(name) {
+  var cats = ['Arnold', ...kittens];
+  return cats;
+}
+function removeLastKitten() {
+  var cats = kittens.slice(0, kittens.length - 1);
+  return cats;
+}
+function removeFirstKitten() {
+  var cats = kittens.slice(1);
+  return cats;
+}
