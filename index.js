3224e5cdf1df957b7a4ddefbd2d43f7d56087c5f
Vicky Adijanto <vickyadijanto@gmail.com>
2017-03-01 02:23:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+function prependKitten(name){
+  return [name,...kittens]
+}
+function removeLastKitten(){
+  var kitCopy=kittens.slice()
+  kitCopy.pop()
+  return kitCopy
+}
+function removeFirstKitten(){
+  var kitCopy=kittens.slice()
+  kitCopy.shift()
+  return kitCopy
+}
